8719e2cdbde572e77705c72763664c2200fa2cab
kss-yoshida <107829330+kss-yoshida@users.noreply.github.com>
2022-07-13 14:11:58 +0900

    Add files via upload

--- EngravingSystem/src/main/java/engraving/system/controller/EngravingController.java
+++ EngravingSystem/src/main/java/engraving/system/controller/EngravingController.java
@@ -2,8 +2,12 @@ package engraving.system.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
 
 import engraving.system.entity.*;
 import engraving.system.repository.*;
@@ -15,7 +19,31 @@ public class EngravingController {
 	@Autowired
 	private AttendanceRepository attendanceinfo;
 	
-	//「engraving」にアクセスがあった場合
+	//「/startEngraving」にアクセスがあった場合
+	@RequestMapping(value = "/startEngraving", method = RequestMethod.POST)
+	public ModelAndView engravingStart(@ModelAttribute Attendance attendance, ModelAndView mav) {
+		//入力された情報をDBに保存
+		attendanceinfo.saveAndFlush(attendance);
+		
+		//リダイレクト先を指定
+		mav = new ModelAndView("redirect;/menu");
+		
+		//ModelとView情報を返す
+		return mav;
+	}
 	
+	//「finishEngraving」にアクセスがあった場合
+	@RequestMapping(value = "/finishEngraving", method = RequestMethod.POST)
+	public ModelAndView finishEngraving(ArrayList finishEngraving, ModelAndView mav) {
+		
+		//入力された情報を更新
+		
+		
+		//リダイレクト先を指定
+		mav = new ModelAndView("redirect:/menu");
+		
+		//ModelとView情報を返す
+		return mav;
+	}
 	
 }
