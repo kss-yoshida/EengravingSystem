dbddf7d7e0b70c1cd4e6aab995db8ddaaaf057e4
kss-yoshida <107829330+kss-yoshida@users.noreply.github.com>
2022-07-14 18:01:29 +0900

    Update AttendanceRepository.java

--- EngravingSystem/src/main/java/engraving/system/repository/AttendanceRepository.java
+++ EngravingSystem/src/main/java/engraving/system/repository/AttendanceRepository.java
@@ -8,4 +8,6 @@ import engraving.system.entity.Attendance;
 @Repository
 public interface AttendanceRepository extends JpaRepository<Attendance, String> {
 
+  //退勤打刻時間とそれに付随するデータを更新する処理
+	public Attendance findByDayAndEmployeeId(String day,String employeeId);
 }
