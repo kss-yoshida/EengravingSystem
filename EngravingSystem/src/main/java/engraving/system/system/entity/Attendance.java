d4a9fc4afc6052b71bb7aa1d9abecd01194d3ca8
Shiraishi-Shunnosuke <107897017+Shiraishi-Shunnosuke@users.noreply.github.com>
2022-07-15 10:45:38 +0900

    Add files via upload

---
+++ EngravingSystem/src/main/java/engraving/system/system/entity/Attendance.java
@@ -0,0 +1,112 @@
+package engraving.system.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="attendanceinfo")
+public class Attendance {
+	
+	//勤怠ID
+	@Id
+	private int attendanceId;
+	
+	public int getAttendanseId() {
+		return attendanceId;
+	}
+	
+	public void setAttendanceId(int attendanceId) {
+		this.attendanceId = attendanceId;
+	}
+	
+	//社員番号
+	@Column(nullable = false)
+	private int employeeId;
+	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	
+	//日付
+	@Column(nullable = false)
+	private String day;
+	
+	public String getDay() {
+		return day;
+	}
+	
+	public void setDay(String day) {
+		this.day = day;
+	}
+	
+	//出勤時間
+	@Column(nullable = false)
+	private String startTime;
+	
+	public String getStartTime() {
+		return startTime;
+	}
+	
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+	
+	//退勤時間
+	private String finishTime;
+	
+	public String getFinishTime() {
+		return finishTime;
+	}
+	
+	public void setFinishTime(String finishTime) {
+		this.finishTime = finishTime;
+	}
+	
+	//出勤時打刻時間
+	@Column(nullable = false)
+	private String startEngrave;
+	
+	public String getStartEngrave() {
+		return startEngrave;
+	}
+	
+	public void setStartEngrave(String startEngrave) {
+		this.startEngrave = startEngrave;
+	}
+	
+	//退勤時打刻時間
+	private String finishEngrave;
+	
+	public String getFinishEngrave() {
+		return finishEngrave;
+	}
+	
+	public void setFinishEngrave(String finishEngrave) {
+		this.finishEngrave = finishEngrave;
+	}
+	
+	//休憩時間
+	private String breakTime;
+	
+	public String getBreakTime() {
+		return breakTime;
+	}
+	
+	public void setBreakTime(String breakTime) {
+		this.breakTime = breakTime;
+	}
+	
+	//残業時間
+	private String overTime;
+	
+	public String getOverTime() {
+		return overTime;
+	}
+	
+	public void setOverTime(String overTime) {
+		this.overTime = overTime;
+	}
+}
---
+++ EngravingSystem/src/main/java/engraving/system/system/entity/User.java
@@ -0,0 +1,80 @@
+package engraving.system.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="userinfo")
+public class User{
+	
+	//employee
+	@Id
+	private int employeeId;
+	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	
+	//name
+	@Column(length = 100, nullable = false)
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	//email
+	@Column(length = 100, nullable = false)
+	private String email;
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	//password
+	@Column(length = 100, nullable = false)
+	private String password;
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	//写真
+	@Column(length = 100)
+	private String photo;
+	
+	public String getPhoto() {
+		return photo;
+	}
+	
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	
+	//削除フラグ
+	@Column(nullable = false)
+	private boolean isDeleted;
+	
+	public boolean getIsDeleted() {
+		return isDeleted;
+	}
+	
+	public void setIsDeleted(boolean isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+}
