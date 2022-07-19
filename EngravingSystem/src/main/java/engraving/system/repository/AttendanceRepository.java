a5da4be81d2838e0fcb45c8282181c08b7e7e03a
kss-yoshida <107829330+kss-yoshida@users.noreply.github.com>
2022-07-19 17:10:06 +0900

    Update AttendanceRepository.java

--- EngravingSystem/src/main/java/engraving/system/repository/AttendanceRepository.java
+++ EngravingSystem/src/main/java/engraving/system/repository/AttendanceRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import engraving.system.entity.Attendance;
 
+import java.util.*;
+
 @Repository
 public interface AttendanceRepository extends JpaRepository<Attendance, String> {
 
