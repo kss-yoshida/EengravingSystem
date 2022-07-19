d7dda06e834cc6f07ee5a60646eb6df1a7b4eab4
kss-yoshida <107829330+kss-yoshida@users.noreply.github.com>
2022-07-19 09:42:39 +0900

    Update EngravingController.java

--- EngravingSystem/src/main/java/engraving/system/controller/EngravingController.java
+++ EngravingSystem/src/main/java/engraving/system/controller/EngravingController.java
@@ -3,6 +3,7 @@ package engraving.system.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.beans.factory.annotation.Autowired;
