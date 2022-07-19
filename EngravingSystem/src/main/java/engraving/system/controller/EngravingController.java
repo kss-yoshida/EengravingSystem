b1ff885f1351677457fecf4c42f73de3da387c9c
Kishibashi0521 <107822977+Kishibashi0521@users.noreply.github.com>
2022-07-19 13:48:11 +0900

    Update EngravingController.java

--- EngravingSystem/src/main/java/engraving/system/controller/EngravingController.java
+++ EngravingSystem/src/main/java/engraving/system/controller/EngravingController.java
@@ -1,10 +1,12 @@
 package engraving.system.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,107 +18,117 @@ import engraving.system.entity.*;
 import engraving.system.repository.*;
 
 @Controller
+@SessionAttributes(value = "user")
 public class EngravingController {
+	@ModelAttribute(value = "user")
+	public User setUpUser() {
+		return new User();
+	}
 
-	//Repositoryインターフェースを自動インスタンス化
+
+	// Repositoryインターフェースを自動インスタンス化
 	@Autowired
 	private AttendanceRepository attendanceinfo;
+	@Autowired
 	private ChangeRepository changeinfo;
+	@Autowired
 	private UserRepository userinfo;
+	@Autowired
 	private LoginLogRepository logininfo;
+	@Autowired
 	HttpSession session;
-	
+
 	/*
-	*出勤退勤処理
-	*/
-	//「/startEngraving」にアクセスがあった場合
+	 * 出勤退勤処理
+	 */
+	// 「/startEngraving」にアクセスがあった場合
 	@RequestMapping(value = "/startEngraving", method = RequestMethod.POST)
 	public ModelAndView engravingStart(ModelAndView mav) {
-		//勤怠情報を格納するAttendanceの作成
+		// 勤怠情報を格納するAttendanceの作成
 		Attendance attendance = new Attendance();
-		User user = (User)session.getAttribute("user");
+		User user = (User) session.getAttribute("user");
 		attendance.setEmployeeId(user.getEmployeeId());
-		//日付の受け取りと格納
+		// 日付の受け取りと格納
 		Date date = new Date();
 		SimpleDateFormat day = new SimpleDateFormat("yyyyMMdd");
 		attendance.setDay(day.format(date));
-		//出勤時間の受け取りと格納
+		// 出勤時間の受け取りと格納
 		SimpleDateFormat time = new SimpleDateFormat("kk:mm");
 		attendance.setStartTime(time.format(date));
 		attendance.setStartEngrave(time.format(date));
-		//情報をDBに保存
+		// 情報をDBに保存
 		attendanceinfo.saveAndFlush(attendance);
-		
-		//リダイレクト先を指定
+
+		// リダイレクト先を指定
 		mav = new ModelAndView("redirect;/menu");
-		
-		//ModelとView情報を返す
+
+		// ModelとView情報を返す
 		return mav;
 	}
-	
-	//「finishEngraving」にアクセスがあった場合
+
+	// 「finishEngraving」にアクセスがあった場合
 	@RequestMapping(value = "/finishEngraving", method = RequestMethod.POST)
 	public ModelAndView finishEngraving(ModelAndView mav) {
 
-		//入力された情報を更新
+		// 入力された情報を更新
 		Date date = new Date();
 		SimpleDateFormat day = new SimpleDateFormat("yyyyMMdd");
-		//ユーザーの当日の打刻情報を呼び出す
-		User user = (User)session.getAttribute("user");
+		// ユーザーの当日の打刻情報を呼び出す
+		User user = (User) session.getAttribute("user");
 		int employeeId = user.getEmployeeId();
 		String strEmployeeId = Integer.toString(employeeId);
-		Attendance attendance =  attendanceinfo.findByDayAndEmployeeId(day.format(date), strEmployeeId);
-		
-		if(attendance.getFinishEngrave() == null) {
-			//退勤の打刻を行う
-			//退勤時間をattendanceに格納
+		Attendance attendance = attendanceinfo.findByDayAndEmployeeId(day.format(date), strEmployeeId);
+
+		if (attendance.getFinishEngrave() == null) {
+			// 退勤の打刻を行う
+			// 退勤時間をattendanceに格納
 			SimpleDateFormat time = new SimpleDateFormat("kk:mm");
 			attendance.setFinishEngrave(time.format(date));
 			attendance.setFinishTime(time.format(date));
-			
-			//勤務時間（時間）の計算
-			int hour = Integer.parseInt(attendance.getStartTime().substring(0,1)) -
-					Integer.parseInt(attendance.getFinishTime().substring(0,1));
-			//勤務時間（分）の計算
-			int minute =  Integer.parseInt(attendance.getStartTime().substring(3,4)) -
-					Integer.parseInt(attendance.getFinishTime().substring(3,4));
-			if(minute < 0) {
+
+			// 勤務時間（時間）の計算
+			int hour = Integer.parseInt(attendance.getStartTime().substring(0, 1))
+					- Integer.parseInt(attendance.getFinishTime().substring(0, 1));
+			// 勤務時間（分）の計算
+			int minute = Integer.parseInt(attendance.getStartTime().substring(3, 4))
+					- Integer.parseInt(attendance.getFinishTime().substring(3, 4));
+			if (minute < 0) {
 				minute = 60 + minute;
-				hour = hour-1;
+				hour = hour - 1;
 			}
-			//休憩時間・残業時間の計算と格納
-			if(hour < 9) {//8時間未満の勤務の場合
+			// 休憩時間・残業時間の計算と格納
+			if (hour < 9) {// 8時間未満の勤務の場合
 				attendance.setBreakTime("01:00");
 				attendance.setOverTime("00:00");
-			}else {//８時間以上の勤務の場合
+			} else {// ８時間以上の勤務の場合
 				attendance.setBreakTime("01:15");
-				//残業時間の計算
+				// 残業時間の計算
 				hour = hour - 9;
 				minute = minute - 15;
-				if(minute < 0) {
+				if (minute < 0) {
 					minute = 60 + minute;
 				}
 				attendance.setOverTime(hour + ":" + minute);
 			}
 		}
-		
+
 		attendanceinfo.saveAndFlush(attendance);
-		
-		//リダイレクト先を指定
+
+		// リダイレクト先を指定
 		mav = new ModelAndView("redirect:/menu");
-		
-		//ModelとView情報を返す
+
+		// ModelとView情報を返す
 		return mav;
 	}
+
 	/*
-	 * ログインの処理
-	 * セッションの設定とログイン履歴の登録
+	 * ログインの処理 セッションの設定とログイン履歴の登録
 	 */
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public ModelAndView login(ModelAndView mav, @RequestParam("password") String pass,
+	public String login(ModelAndView mav, @RequestParam("password") String pass,
 			@RequestParam("employee_id") int num) {
 //		ユーザーリストの取得
-		ArrayList<User> list = (ArrayList<User>)userinfo.findAll();
+		ArrayList<User> list = (ArrayList<User>) userinfo.findAll();
 		User user = new User();
 		LoginLog log = new LoginLog();
 		String cmd = "";
@@ -128,10 +140,10 @@ public class EngravingController {
 //				ログイン履歴の設定
 				log.setEmployee_id(user.getEmployeeId());
 				Date date = new Date();
-				SimpleDateFormat format = new SimpleDateFormat("YY-MM-dd HH-mm-ss"); //年-月-日 時-分-秒
+				SimpleDateFormat format = new SimpleDateFormat("YY-MM-dd HH-mm-ss"); // 年-月-日 時-分-秒
 				String datetime = format.format(date);
 				log.setLogin_time(datetime);
-				
+
 //				セッションの設定
 				session.setAttribute("user", user);
 //				ログイン履歴をDBに追加
@@ -140,15 +152,16 @@ public class EngravingController {
 				cmd = "ok";
 			}
 		}
-		
+
 		if (cmd.equals("")) {
 //			エラーがあったらログインに戻る
 			mav = new ModelAndView("redirect;/login");
+			return "login";
+
 		} else {
 //			OKになってたらメニューに行く
-			mav = new ModelAndView("redirect;/menu");
+			return "employeeMenu";
 		}
-		return mav;
 
 	}
 
@@ -158,10 +171,9 @@ public class EngravingController {
 
 		return mav;
 	}
-	
+
 	@RequestMapping("/employeeMenu")
 	public ModelAndView employeeMenu(ModelAndView mav) {
-		mav.setViewName("employeeMenu");
 		return mav;
 	}
 
@@ -170,5 +182,5 @@ public class EngravingController {
 		mav.setViewName("adminMenu");
 		return mav;
 	}
-	
+
 }
