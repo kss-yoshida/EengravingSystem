41ee784c6752aeabfc1cd659815f7b879e0511f2
Kishibashi0521 <107822977+Kishibashi0521@users.noreply.github.com>
2022-07-21 15:46:57 +0900

    Update UserRepository.java

--- EngravingSystem/src/main/java/engraving/system/repository/UserRepository.java
+++ EngravingSystem/src/main/java/engraving/system/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package engraving.system.repository;
 
+import java.util.ArrayList;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,11 @@ import engraving.system.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+
+	public ArrayList<User> findByEmployeeId(int id);
+
+	public ArrayList<User> findByName(String name);
+
+	public ArrayList<User> findByEmployeeIdAndName(int id, String name);
 
 }
