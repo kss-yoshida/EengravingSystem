ad93dd9e4e90a51b87b900b656d6558bf56dc22d
kss-yoshida <107829330+kss-yoshida@users.noreply.github.com>
2022-07-26 16:45:27 +0900

    Add files via upload

---
+++ EngravingSystem/src/main/java/engraving/system/entity/RequestDay.java
@@ -0,0 +1,113 @@
+package engraving.system.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class RequestDay {
+
+//	リクエストID
+	@Id
+	@Column(name="request_id")
+	private int requestId;
+	
+	public void setRequestId(int id) {
+		this.requestId=id;
+	}
+	public int getRequestId() {
+		return this.requestId;
+	}
+	
+//	社員番号
+	@Column(name="employee_id" ,nullable=false)
+	private String employeeId;
+	
+	public void setEmployeeId(String id) {
+		this.employeeId=id;
+	}
+	public String getEmployeeId() {
+		return this.employeeId;
+	}
+	
+//	変更希望の勤怠情報ID
+	@Column(name="attendance_id" ,nullable=false)
+	private int attendanceId;
+	public void setAttendanceId(int id) {
+		this.attendanceId=id;
+	}
+	public int getAttendanceId() {
+		return this.attendanceId;
+	}
+	
+	//日付
+	@Column(name="day" , nullable=false)
+	private String day;
+	
+	public void setDay(String day) {
+		this.day = day;
+	}
+	
+	public String getDay() {
+		return day;
+	}
+	
+//	変更前出勤時間
+	@Column(nullable=true)
+	private String beforeStartEngrave;
+	public void setBeforeStartEngrave(String time) {
+		this.beforeStartEngrave=time;
+	}
+	public String getBeforeStartEngrave() {
+		return this.beforeStartEngrave;
+	}
+	
+//	変更前退勤時間
+	@Column(nullable=true)
+	private String beforeFinishEngrave;
+	public void setBeforeFinishEngrave(String time) {
+		this.beforeFinishEngrave=time;
+	}
+	public String getBeforeFinishEngrave() {
+		return this.beforeFinishEngrave;
+	}
+	
+//	変更希望出勤時間
+	@Column(name="change_start_engrave" ,nullable=true)
+	private String changeStartEngrave;
+	public void setChangeStartEngrave(String time) {
+		this.changeStartEngrave=time;
+	}
+	public String getChangeStartEngrave() {
+		return this.changeStartEngrave;
+	}
+	
+//	変更希望退勤時間
+	@Column(name="change_finish_engrave" ,nullable=true)
+	private String changeFinishEngrave;
+	public void setChangeFinishEngrave(String time) {
+		this.changeFinishEngrave=time;
+	}
+	public String getChangeFinishEngrave() {
+		return this.changeFinishEngrave;
+	}
+	
+//	変更希望理由
+	@Column(name="comment" ,nullable=false)
+	private String comment;
+	public void setComment(String comment) {
+		this.comment=comment;
+	}
+	public String getComment() {
+		return this.comment;
+	}
+
+	
+//	削除フラグ
+	@Column(name="is_deleted" ,nullable=false)
+	private Boolean isDeleted;
+	public void setIsDeleted(Boolean flag) {
+		this.isDeleted=flag;
+	}
+	public Boolean getIsDeleted() {
+		return this.isDeleted;
+	}
+}
