512655c4bb50b64a305ef554fb59a67c892b7fed
kss-yoshida <107829330+kss-yoshida@users.noreply.github.com>
2022-07-27 12:03:23 +0900

    Add files via upload

--- EngravingSystem/src/main/java/engraving/system/repository/ChangeRepository.java
+++ EngravingSystem/src/main/java/engraving/system/repository/ChangeRepository.java
@@ -3,9 +3,20 @@ package engraving.system.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 import engraving.system.entity.Change;
 
 @Repository
 public interface ChangeRepository extends JpaRepository<Change, String> {
-
+	//ユーザーIDで検索する
+	public ArrayList<Change> findByEmployeeIdLike(String employeeId);
+	
+	//管理者IDで検索する
+	public ArrayList<Change> findByAdminIdLike(String adminId);
+	
+	//ユーザーIDと管理者IDで検索する
+	public ArrayList<Change> findByEmployeeIdAndAdminIdLike(String employeeId, String adminId);
+	
+	
 }
