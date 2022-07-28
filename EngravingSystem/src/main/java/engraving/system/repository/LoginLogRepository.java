0b688fd2795d60c0697a8f83014d3e2b4907c940
kss-yoshida <107829330+kss-yoshida@users.noreply.github.com>
2022-07-28 16:04:58 +0900

    Update LoginLogRepository.java

--- EngravingSystem/src/main/java/engraving/system/repository/LoginLogRepository.java
+++ EngravingSystem/src/main/java/engraving/system/repository/LoginLogRepository.java
@@ -3,6 +3,8 @@ package engraving.system.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 import engraving.system.entity.LoginLog;
 
 @Repository
